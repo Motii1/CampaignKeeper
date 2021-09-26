857168693ed72176c4c4c6531593b23c09d18061
Dawid Motak <dawidmotak1@gmail.com>
2021-09-26 22:27:27 +0200

    chore: initial frontend setup

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// proxying all requests matching /app/* routes in development environment
+module.exports = app => {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+      pathRewrite: path => path.replace('/api', ''),
+    })
+  );
+};
