97c29104ae62f67dba4e6dd5af3dff2b7c8d7587
Dawid Motak <dawidmotak1@gmail.com>
2021-10-25 15:03:14 +0200

    feat: declare AuthController for app

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import { IApp } from './Application/App/IApp';
+import { logger } from './Common/Logger/Logger';
 
 export class Server {
   constructor(private readonly app: IApp) {}
 
-  run(): void {
+  async run(): Promise<void> {
+    await createConnection();
+    logger.info('Established database connection');
     this.app.start();
   }
 }
