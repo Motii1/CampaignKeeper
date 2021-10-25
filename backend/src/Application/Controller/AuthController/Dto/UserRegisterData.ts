6c21f82f5117021806ef485a51c25663b4da5bc1
Dawid Motak <dawidmotak1@gmail.com>
2021-10-25 20:48:39 +0200

    refactor: change username validation rules

--- backend/src/Application/Controller/AuthController/Dto/UserRegisterData.ts
+++ backend/src/Application/Controller/AuthController/Dto/UserRegisterData.ts
@@ -5,7 +5,7 @@ const passwordValidationPattern =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,255}$/;
 
 export const userRegisterDataSchema = Joi.object<UserRegisterData>({
-  username: Joi.string().max(255).required(),
+  username: Joi.string().min(3).max(32).required(),
   password: Joi.string().max(255).pattern(passwordValidationPattern).required(),
   repeatedPassword: Joi.string().required().equal(Joi.ref('password')),
   email: Joi.string().email().required(),
