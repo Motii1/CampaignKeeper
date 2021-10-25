32cef3150029a4c2d4ae2ec9d4063d7943e2807f
Dawid Motak <dawidmotak1@gmail.com>
2021-10-25 15:04:40 +0200

    feat: define user infrastracture entity

---
+++ backend/src/Infrastracture/Entity/User/Mapping.ts
@@ -0,0 +1,9 @@
+import { User } from '../../../Domain/User/User';
+import { UserEntity } from './UserEntity';
+
+export const mapEntityToDomainObject = (entity: UserEntity): User => ({
+  username: entity.username,
+  passwordHash: entity.passwordHash,
+  email: entity.email,
+  id: entity.id,
+});
