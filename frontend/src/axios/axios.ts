a7eb08aafdc40f9118dceec5dd7ffa723a1e8f2c
PrzeSta <przemyslaw.stasiuk98@gmail.com>
2021-11-13 15:43:36 +0100

    added timeout setting to axios

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const protectedApiClient = axios.create({
   validateStatus: status => status < 500,
+  timeout: 30000,
 });
 
 protectedApiClient.interceptors.response.use(
