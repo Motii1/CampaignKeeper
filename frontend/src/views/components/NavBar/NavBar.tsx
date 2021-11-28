03ea3cca864d0a399922781d7319eea279bde26f
Przemyslaw Stasiuk <przemyslaw.stasiuk98@gmail.com>
2021-11-28 15:33:30 +0100

    big buttons on NavBar finished

--- frontend/src/views/components/NavBar/NavBar.tsx
+++ frontend/src/views/components/NavBar/NavBar.tsx
@@ -1,13 +1,13 @@
 import { Paper, Stack } from '@mui/material';
 import { useHistory } from 'react-router';
-import { BigNavBarButton, Logo } from './elements';
+import { BigNavBarButton, Logo, LogoutPanel } from './elements';
 
 // how to pass info which button should be chosen to NavBar?
 // how to disable onClick in button when it is chosen?
 export const NavBar: React.FC = () => {
   const history = useHistory();
   return (
-    <Paper sx={{ backgroundColor: 'customBackgrounds.lightGray', maxHeight: 60 }} square>
+    <Paper sx={{ backgroundColor: 'customBackgrounds.lightGray', height: 50 }} square>
       <Stack direction="row" spacing={0} justifyContent="flex-start" alignItems="flex-start">
         <Logo />
         <BigNavBarButton text="START" isChosen={true} onClick={() => history.push('/welcome')} />
@@ -17,6 +17,7 @@ export const NavBar: React.FC = () => {
           onClick={() => history.push('/campaign')}
         />
         <BigNavBarButton text="NOTES" isChosen={false} onClick={() => history.push('/notes')} />
+        <LogoutPanel onClick={() => history.push('/welcome')} />
       </Stack>
     </Paper>
   );
--- frontend/src/views/components/NavBar/elements.tsx
+++ frontend/src/views/components/NavBar/elements.tsx
@@ -1,4 +1,5 @@
-import { Container, Paper, Typography } from '@mui/material';
+import { AccountCircle, MoreVert } from '@mui/icons-material';
+import { Container, Paper, Stack, Typography } from '@mui/material';
 import LogoGraphic from '../../../graphics/logo.svg';
 
 type NavBarButtonProps = {
@@ -13,9 +14,10 @@ export const Logo: React.FC = () => (
       backgroundImage: `url(${LogoGraphic})`,
       backgroundSize: 'contain',
       backgroundRepeat: 'no-repeat',
-      height: 120,
-      marginLeft: 5,
-      marginRight: 5,
+      height: 100,
+      width: 260,
+      marginLeft: 2,
+      marginRight: 2,
       overflow: 'visible',
       zIndex: 1,
     }}
@@ -28,12 +30,23 @@ export const BigNavBarButton: React.FC<NavBarButtonProps> = props => {
     return (
       <Paper
         elevation={0}
-        sx={{ backgroundColor: 'customColors.gold', height: 80, overflow: 'visible', zIndex: 1 }}
+        sx={{
+          backgroundColor: 'customColors.gold',
+          height: 60,
+          overflow: 'visible',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
         onClick={props.onClick}
         square
       >
-        <Typography variant="h5" align="center" sx={{ color: 'common.black', margin: 5 }}>
-          {props.text}
+        <Typography
+          variant="h5"
+          sx={{ cursor: 'pointer', color: 'common.black', paddingLeft: 3, paddingRight: 3 }}
+        >
+          <b>{props.text}</b>
         </Typography>
       </Paper>
     );
@@ -42,14 +55,19 @@ export const BigNavBarButton: React.FC<NavBarButtonProps> = props => {
       elevation={0}
       sx={{
         backgroundColor: 'customBackgrounds.lightGray',
-        height: 'dimensions.navBarHeight',
-        width: 'auto',
+        height: 50,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
       onClick={props.onClick}
       square
     >
-      <Typography variant="h6" align="center" sx={{ color: 'common.white' }}>
-        {props.text}
+      <Typography
+        variant="h6"
+        sx={{ cursor: 'pointer', color: 'common.white', paddingLeft: 2, paddingRight: 2 }}
+      >
+        <b>{props.text}</b>
       </Typography>
     </Paper>
   );
@@ -61,3 +79,32 @@ export const SmallNavBarButton = (props: NavBarButtonProps): React.FC => {
 
 } 
 */
+
+type LogoutProps = {
+  onClick: (event: React.MouseEvent<SVGSVGElement>) => void;
+};
+
+export const LogoutPanel: React.FC<LogoutProps> = props => (
+  <Paper
+    elevation={0}
+    sx={{
+      backgroundColor: 'customColors.gold',
+      height: 50,
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      paddingLeft: 1,
+      paddingRight: 1,
+      marginLeft: 'auto',
+    }}
+    square
+  >
+    <Stack direction="row" spacing={1} justifyContent="flex-start" alignItems="flex-start">
+      <AccountCircle sx={{ color: 'common.black' }} />
+      <Typography variant="subtitle1" sx={{ color: 'common.black' }}>
+        <b>{'PhenoilGray2137'}</b>
+      </Typography>
+      <MoreVert onClick={props.onClick} sx={{ color: 'common.black' }} />
+    </Stack>
+  </Paper>
+);
