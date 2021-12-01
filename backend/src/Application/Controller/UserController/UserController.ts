2d427556a43b22ced6c4eb1ab3bb5abf0f09dffd
Dawid Motak <dawidmotak1@gmail.com>
2021-12-01 22:41:46 +0100

    feat: add user controller

---
+++ backend/src/Application/Controller/UserController/UserController.ts
@@ -0,0 +1,78 @@
+import { Request, Response, Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import { handleUserImagePersistance } from '../../../Domain/User/Service/Image';
+import { authorization } from '../../Middleware/Auth/Authorization';
+import { buildSingleImageUploader } from '../../Middleware/Upload/Image';
+import { extractUserFromCookies } from '../../Util/Authorization';
+import { IController } from '../IController';
+
+const MAX_FILE_SIZE_LIMIT = 500000;
+const IMAGE_FILE_FIELD_NAME = 'image-file';
+
+enum UserRoutes {
+  Details = '/details',
+  Image = '/image',
+}
+
+export class UserController implements IController {
+  private router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.declareRoutes();
+  }
+
+  private declareRoutes = (): void => {
+    this.router.get(UserRoutes.Image, authorization, asyncHandler(this.getImageHandler));
+    this.router.put(UserRoutes.Image, authorization, asyncHandler(this.uploadImageHandler));
+    this.router.get(UserRoutes.Details, authorization, asyncHandler(this.detailsHandler));
+  };
+
+  /**
+   * @route GET /user/image
+   * @group user - Operations related to user data
+   * @produces image/*
+   * @returns {file} 200 - User avatar or placeholder if avatar is not set
+   * @security cookieAuth
+   */
+  private getImageHandler = async (req: Request, res: Response): Promise<void> => {
+    const user = await extractUserFromCookies(req.cookies);
+    res.end(user!.image!, 'binary');
+  };
+
+  /**
+   * @route PUT /user/image
+   * @group user - Operations related to user data
+   * @consumes multipart/form-data
+   * @param {file} image-file.formData - image
+   * @returns {EmptyResponse.model} 200 - Image succesfully saved
+   * @returns {Message.model} 400 - Invalid file provided
+   * @security cookieAuth
+   */
+  private uploadImageHandler = async (req: Request, res: Response): Promise<void> => {
+    const { file, error } = await buildSingleImageUploader(
+      MAX_FILE_SIZE_LIMIT,
+      IMAGE_FILE_FIELD_NAME
+    )(req, res);
+    if (error) {
+      res.status(400).json({ message: error.message });
+      return;
+    }
+    const user = await extractUserFromCookies(req.cookies);
+    await handleUserImagePersistance(file!, user);
+    res.status(200).json({});
+  };
+
+  /**
+   * @route GET /user/details
+   * @group user - Operations related to user data
+   * @returns {UserInformation.model} 200 - User details
+   * @security cookieAuth
+   */
+  private detailsHandler = async (req: Request, res: Response): Promise<void> => {
+    const user = await extractUserFromCookies(req.cookies);
+    res.status(200).json({ username: user!.username, email: user!.email });
+  };
+
+  getRouter = (): Router => this.router;
+}
