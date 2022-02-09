0325c55d2d107bc3374f6a8c30c53cd5a40c874d
Dawid Motak <dawidmotak1@gmail.com>
2022-02-09 22:21:57 +0100

    feat: define campaign entity

---
+++ backend/src/Infrastracture/Entity/Campaign/CampaignEntity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { SchemaEntity } from '../Schema/SchemaEntity';
+import { SessionEntity } from '../Session/SessionEntity';
+import { UserEntity } from '../User/UserEntity';
+
+@Entity({ name: 'campaign' })
+export class CampaignEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ length: 128 })
+  name!: string;
+
+  @ManyToOne(() => UserEntity, user => user.campaigns, { nullable: false })
+  user!: UserEntity;
+
+  @OneToMany(() => SchemaEntity, schema => schema.campaign)
+  schemas!: SchemaEntity[];
+
+  @OneToMany(() => SessionEntity, session => session.campaign)
+  sessions!: SessionEntity[];
+
+  @Column({ type: 'varbinary', nullable: true, length: 'max' })
+  image!: Buffer | null;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
