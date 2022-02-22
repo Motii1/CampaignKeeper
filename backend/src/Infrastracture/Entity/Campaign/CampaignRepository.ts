93dd9997d85494fe329ad4b9970efda85e252911
Dawid Motak <dawidmotak1@gmail.com>
2022-02-22 22:33:04 +0100

    feat: add campaign repository

---
+++ backend/src/Infrastracture/Entity/Campaign/CampaignRepository.ts
@@ -0,0 +1,44 @@
+// export const findUserByIdentityString = async (identity: string): Promise<User | null> => {
+//   const userRepository = getRepository(UserEntity);
+//   const entity = await userRepository.findOne({
+//     where: [{ username: identity }, { email: identity }],
+//   });
+//   if (!entity) {
+//     return null;
+//   }
+
+import { getRepository } from 'typeorm';
+import { Campaign } from '../../../Domain/Campaign/Campaign';
+import { User } from '../../../Domain/User/User';
+import { CampaignEntity } from './CampaignEntity';
+import { mapEntityToDomainObject } from './Mapping';
+
+export const findAllUserCampaigns = async (user: User): Promise<Campaign[]> => {
+  const entities = await getRepository(CampaignEntity).find({ where: { user: { id: user.id } } });
+  return entities.map(mapEntityToDomainObject);
+};
+
+export const findUserCampaignById = async (id: number, user: User): Promise<Campaign | null> => {
+  const campaign = await getRepository(CampaignEntity).findOne({
+    where: { id, user: { id: user.id } },
+  });
+  if (!campaign) {
+    return null;
+  }
+  return mapEntityToDomainObject(campaign);
+};
+
+export const updateCampaignImageById = async (image: Buffer | null, id: number): Promise<void> => {
+  await getRepository(CampaignEntity).update({ id }, { image });
+};
+
+export const deleteUserCampaignById = async (id: number, user: User): Promise<void> => {
+  await getRepository(CampaignEntity).delete({ id, user: { id: user.id } });
+};
+
+export const saveCampaign = async (
+  campaign: Omit<Campaign, 'id' | 'createdAt'>
+): Promise<Campaign> => {
+  const entity = await getRepository(CampaignEntity).save(campaign);
+  return mapEntityToDomainObject(entity);
+};
