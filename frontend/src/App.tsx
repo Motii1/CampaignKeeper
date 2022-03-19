e31151ad023d6d74dcbbd527fde066976a010fa8
Przemyslaw Stasiuk <przemyslaw.stasiuk98@gmail.com>
2022-03-19 12:37:32 +0100

    added fetching campaigns list to App render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,14 @@ import { ErrorBoundary } from './ErrorBoundary';
 import { Routing } from './Routing/Routing';
 import { theme } from './theme/theme';
 import { fetchUserDetails } from './views/LandingView/userDetailsSlice';
+import { fetchCampaigns } from './views/StartView/campaignsSlice';
 
 export const App: React.FC = () => {
   const dispatch = useDispatch();
   dispatch(fetchUserDetails());
+  dispatch(fetchCampaigns());
+  // eslint-disable-next-line no-console
+  console.log('App');
 
   return (
     <BrowserRouter>
