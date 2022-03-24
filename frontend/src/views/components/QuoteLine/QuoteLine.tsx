b0e741627a0ef496549b823836f44241333f9a9c
maniman303 <maniman303@gmail.com>
2022-03-24 13:01:33 +0100

    Small ui tweaks

--- frontend/src/views/components/QuoteLine/QuoteLine.tsx
+++ frontend/src/views/components/QuoteLine/QuoteLine.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 type QuoteLineProps = {
   text: string;
@@ -7,21 +7,32 @@ type QuoteLineProps = {
 // TO-DO: decide if quotes should be fetched from backend or imported
 // TO-DO: establish contraints on quote length
 export const QuoteLine: React.FC<QuoteLineProps> = props => (
-  <Typography
-    noWrap
-    align="center"
-    variant="h6"
+  <Box
+    component="div"
     sx={{
-      color: 'customPalette.accent',
-      fontStyle: 'italic',
-      padding: 1,
-      paddingTop: 7,
-      paddingBottom: 0,
-      height: 55,
-      fontWeight: 'regular',
-      width: '95%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 80,
+      width: '100%',
     }}
   >
-    {props.text}
-  </Typography>
+    <Typography
+      noWrap
+      align="center"
+      variant="h6"
+      sx={{
+        color: 'customPalette.accent',
+        fontStyle: 'italic',
+        padding: 1,
+        paddingTop: 7,
+        paddingBottom: 0,
+        height: 55,
+        fontWeight: 'regular',
+        width: '95%',
+      }}
+    >
+      {props.text}
+    </Typography>
+  </Box>
 );
