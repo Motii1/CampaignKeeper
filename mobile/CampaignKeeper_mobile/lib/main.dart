0306cff83d0616d9a628549aab7595f8fc75d690
maniman303 <maniman303@gmail.com>
2022-04-05 23:01:31 +0200

    Refactor themes

--- mobile/CampaignKeeper_mobile/lib/main.dart
+++ mobile/CampaignKeeper_mobile/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:campaign_keeper_mobile/pages/search.dart';
 import 'package:campaign_keeper_mobile/pages/campaign.dart';
 import 'package:campaign_keeper_mobile/pages/start.dart';
 import 'package:campaign_keeper_mobile/pages/login.dart';
-import 'package:campaign_keeper_mobile/themes/main_themes.dart';
+import 'package:campaign_keeper_mobile/themes/keeper_themes.dart';
 import 'package:campaign_keeper_mobile/pages/loading.dart';
 import 'package:campaign_keeper_mobile/pages/settings.dart';
 import 'package:flutter/services.dart';
@@ -20,8 +20,8 @@ class MainApp extends StatelessWidget {
   MainApp({Key? key}) : super(key: key);
 
   static ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.dark);
-  final ThemeData _theme = MainThemes.light;
-  final ThemeData _themeDark = MainThemes.dark;
+  final ThemeData _theme = KeeperThemes.light;
+  final ThemeData _themeDark = KeeperThemes.dark;
 
   @override
   Widget build(BuildContext context) {
