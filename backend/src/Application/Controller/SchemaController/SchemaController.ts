1a2580c328001a6a9a3f5a4c0b159a6b882a244f
Dawid Motak <dawidmotak1@gmail.com>
2022-05-02 03:19:48 +0200

    feat: add schema controller

--- backend/src/Application/Controller/SchemaController/SchemaController.ts
+++ backend/src/Application/Controller/SchemaController/SchemaController.ts
@@ -1,7 +1,19 @@
 import { Request, Response, Router } from 'express';
 import asyncHandler from 'express-async-handler';
+import { Schema } from '../../../Domain/Campaign/Schema/Schema';
+import { deleteSchema, DeleteSchemaError } from '../../../Domain/Campaign/Schema/Service/Delete';
+import { User } from '../../../Domain/User/User';
+import { findUserCampaignById } from '../../../Infrastracture/Entity/Campaign/CampaignRepository';
+import {
+  findSchemaById,
+  findSchemasByCampaignId,
+  saveSchema,
+} from '../../../Infrastracture/Entity/Schema/SchemaRepository';
 import { authorization } from '../../Middleware/Auth/Authorization';
+import { extractUserFromCookies } from '../../Util/Authorization';
 import { IController } from '../IController';
+import { getSchemaQuerySchema } from './Dto/GetSchemaListQuery';
+import { SchemaInsertDto, schemaInsertDtoSchema } from './Dto/SchemaInsertDto';
 
 enum SchemaRoutes {
   List = '/list',
@@ -16,7 +28,6 @@ export class SchemaContoller implements IController {
   }
 
   private declareRoutes = (): void => {
-    this.router.patch('/:id', authorization, asyncHandler(this.updateSchemaHandler));
     this.router.post('/', authorization, asyncHandler(this.insertSchemaHandler));
     this.router.delete('/:id', authorization, asyncHandler(this.deleteSchemaHandler));
     this.router.get(SchemaRoutes.List, authorization, asyncHandler(this.getSchemasHandler));
@@ -32,44 +43,54 @@ export class SchemaContoller implements IController {
    * @security cookieAuth
    */
   private getSchemasHandler = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json({
-      schemas: [
-        {
-          id: 1,
-          title: 'NPC',
-          deletable: false,
-          fields: ['Name', 'Friends', 'Race', 'Look', 'Affilations'],
-        },
-        { id: 2, title: 'Hero', deletable: true, fields: ['Name', 'Items', 'Skills'] },
-      ],
-    });
+    const { error, value } = getSchemaQuerySchema.validate(req.query);
+    if (error) {
+      res.status(400).json({});
+      return;
+    }
+    const { campaignId } = value;
+    const user = await extractUserFromCookies(req.cookies);
+    const campaign = await findUserCampaignById(campaignId, user);
+    if (!campaign) {
+      res.status(404).json({});
+      return;
+    }
+
+    const schemas = await findSchemasByCampaignId(campaignId);
+    res.status(200).json({ schemas });
   };
 
   /**
    * @route DELETE /schema/{id}
    * @group schema - Operations related to schema data
    * @returns {EmptyResponse.model} 200 - Schema successfully deleted
-   * @returns {EmptyResponse.model} 400 - Wrong schema id
+   * @returns {Message.model} 400 - Wrong schema id or deletion error
    * @returns {EmptyResponse.model} 404 - User schema not found
    * @security cookieAuth
    */
   private deleteSchemaHandler = async (req: Request, res: Response): Promise<void> => {
-    // @todo schema deleted!!
-    res.status(200).json({});
-  };
-
-  /**
-   * @route PATCH /schema/{id}
-   * @group schema - Operations related to schema data
-   * @param {SchemaUpdateDto.model} data.body.required - schema update data
-   * @returns {EmptyResponse.model} 200 - Schema successfully saved
-   * @returns {EmptyResponse.model} 400 - Data in wrong format
-   * @returns {EmptyResponse.model} 404 - Schema not found
-   * @security cookieAuth
-   */
-  private updateSchemaHandler = async (req: Request, res: Response): Promise<void> => {
-    // @todo schema updated!!!
-    res.status(200).json({});
+    const id = Number(req.params.id);
+    if (!this.isSchemaIdValid(id)) {
+      res.status(400).json({ message: 'Given id does not match correct format' });
+      return;
+    }
+    const user = await extractUserFromCookies(req.cookies);
+    const schema = await findSchemaById(id);
+    const permission = await this.permissionsToSchema(schema, user);
+    if (!permission) {
+      res.status(404).json({});
+      return;
+    }
+    try {
+      await deleteSchema(id);
+      res.status(200).json({});
+    } catch (error) {
+      if (error instanceof DeleteSchemaError) {
+        res.status(400).json({ message: error.message });
+        return;
+      }
+      throw error;
+    }
   };
 
   /**
@@ -82,13 +103,30 @@ export class SchemaContoller implements IController {
    * @security cookieAuth
    */
   private insertSchemaHandler = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).json({
-      id: 1,
-      title: 'NPC',
-      deletable: false,
-      fields: ['Name', 'Friends', 'Race', 'Look', 'Affilations'],
-    });
+    const { error, value } = schemaInsertDtoSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({});
+      return;
+    }
+    const dto = value as SchemaInsertDto;
+    const user = await extractUserFromCookies(req.cookies);
+    const campaign = await findUserCampaignById(dto.campaignId, user);
+    if (!campaign) {
+      res.status(404).json({});
+    }
+    const schema = await saveSchema(dto);
+    res.status(200).json(schema);
   };
+
+  private permissionsToSchema = async (schema: Schema | null, user: User): Promise<boolean> => {
+    if (!schema) {
+      return false;
+    }
+    const campaign = await findUserCampaignById(schema.campaignId, user);
+    return !!campaign;
+  };
+
+  private isSchemaIdValid = (id: number): boolean => !isNaN(id) && id > 0;
 
   getRouter = (): Router => this.router;
 }
