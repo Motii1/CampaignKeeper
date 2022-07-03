80a395312d64d8478be9f21ba7550325bb7f6221
Przemyslaw Stasiuk <przemyslaw.stasiuk98@gmail.com>
2022-07-03 16:37:11 +0200

    added selecting new parent during event deletion

--- frontend/src/views/MapView/secondaryDialog/MapSecondaryDialog.tsx
+++ frontend/src/views/MapView/secondaryDialog/MapSecondaryDialog.tsx
@@ -1,12 +1,18 @@
-import { Box, Typography } from '@mui/material';
-import { useCallback, useEffect } from 'react';
+import { Box, MenuItem, SelectChangeEvent, Stack, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import requestMethods from '../../../axios/requestMethods';
 import { useQuery } from '../../../axios/useQuery';
 import { RootState } from '../../../store';
+import { compareEventsByTitle } from '../../../utils/utils';
 import { CustomDialog } from '../../components/CustomDialog/CustomDialog';
+import { CustomSelect } from '../../components/CustomSelect/CustomSelect';
 import { deleteEvent } from '../eventsSlice';
 import { setCurrentEvent } from '../mapViewSlice';
+
+type deleteEventData = {
+  parentId: string;
+};
 
 type MapSecondaryDialogProps = {
   isOpen: boolean;
@@ -19,13 +25,16 @@ type MapSecondaryDialogProps = {
 export const MapSecondaryDialog: React.FC<MapSecondaryDialogProps> = props => {
   const dispatch = useDispatch();
   const eventId = useSelector((state: RootState) => state.mapView.currentEvent?.id);
+  const { eventsList } = useSelector((state: RootState) => state.events);
+
+  const [newParent, setNewParent] = useState<string | null>(null);
 
   const {
     isLoading: isLoadingDelete,
     status: statusDelete,
     runQuery: runQueryDelete,
     resetQuery: resetQueryDelete,
-  } = useQuery(`/api/event/${eventId}`, requestMethods.DELETE);
+  } = useQuery<deleteEventData>(`/api/event/${eventId}`, requestMethods.DELETE);
 
   const handleRunQueryDelete = useCallback(() => {
     if (!isLoadingDelete && statusDelete) {
@@ -47,8 +56,30 @@ export const MapSecondaryDialog: React.FC<MapSecondaryDialogProps> = props => {
     handleRunQueryDelete();
   }, [handleRunQueryDelete]);
 
+  const handleChange = (event: SelectChangeEvent) => {
+    setNewParent(event.target.value);
+  };
+
+  const renderValue = (value: string) => {
+    if (value !== '') return eventsList.find(event => event.id === value)?.title;
+    return 'Choose parent';
+  };
+
+  const renderItems = () => {
+    const possibleNewParents = eventsList
+      .filter(event => event.id !== eventId)
+      .sort(compareEventsByTitle)
+      .map(event => (
+        <MenuItem value={event.id} key={event.id}>
+          {event.title}
+        </MenuItem>
+      ));
+
+    return possibleNewParents;
+  };
+
   const handleOk = () => {
-    runQueryDelete();
+    if (newParent) runQueryDelete({ parentId: newParent });
   };
 
   const handleCancel = () => {
@@ -64,9 +95,26 @@ export const MapSecondaryDialog: React.FC<MapSecondaryDialogProps> = props => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Typography variant="h6" sx={{ color: 'customPalette.onSurface', fontWeight: 'bold' }}>
-          {"This action can't be undone."}
-        </Typography>
+        <Stack direction="column" justifyContent="flex-start" alignItems="flex-start" spacing={1}>
+          {eventsList.find(event => event.id === eventId)?.childrenIds !== [] ? (
+            <>
+              <Typography variant="subtitle1" sx={{ color: 'customPalette.onSurface' }}>
+                Select new parent for children events:
+              </Typography>
+              <CustomSelect
+                labelId={'event-new-parent-select'}
+                handleChange={handleChange}
+                value={newParent ? newParent : ''}
+                renderValue={renderValue}
+              >
+                {renderItems()}
+              </CustomSelect>
+            </>
+          ) : null}
+          <Typography variant="h6" sx={{ color: 'customPalette.onSurface', fontWeight: 'bold' }}>
+            {"This action can't be undone."}
+          </Typography>
+        </Stack>
       </CustomDialog>
     </Box>
   );
