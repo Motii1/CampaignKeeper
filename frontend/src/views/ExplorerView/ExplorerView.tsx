d938736abaab7a79871b37ff441ccd31e3140e64
Przemyslaw Stasiuk <przemyslaw.stasiuk98@gmail.com>
2022-07-11 19:44:25 +0200

    added default event selecting to ExplorerView

--- frontend/src/views/ExplorerView/ExplorerView.tsx
+++ frontend/src/views/ExplorerView/ExplorerView.tsx
@@ -1,20 +1,23 @@
 import { Box, Stack } from '@mui/material';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { RootState } from '../../store';
 import { EventTileType, NavBarViewDialog } from '../../types/types';
 import { EmptyPlaceholder } from '../components/EmptyPlaceholder/EmptyPlaceholder';
 import { EventTile } from '../components/EventTile/EventTile';
 import { ViewWithNavBarWrapper } from '../components/ViewWithNavBarWrapper/ViewWithNavBarWrapper';
 import { SessionEventWithPos } from '../MapView/eventsSlice';
+import viewsRoutes from '../viewsRoutes';
 import { BackButton } from './components/BackButton/BackButton';
 import { ParentDialog } from './components/ParentsDialog/ParentsDialog';
 import { setCurrentEvent } from './explorerViewSlice';
 
 export const ExplorerView: React.FC = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
 
-  const { currentEvent } = useSelector((state: RootState) => state.explorerView);
+  const { currentEvent, currentSessionId } = useSelector((state: RootState) => state.explorerView);
   const { eventsList } = useSelector((state: RootState) => state.events);
 
   const [isOpen, setIsOpen] = useState(false);
@@ -22,6 +25,12 @@ export const ExplorerView: React.FC = () => {
 
   const [isParentDialogOpen, setIsParentDialogOpen] = useState(false);
   const [parentEvents, setParentEvents] = useState<SessionEventWithPos[]>([]);
+
+  if (currentSessionId === '') history.push(viewsRoutes.CAMPAIGN);
+  else if (!currentEvent) {
+    const rootEvent = eventsList.find(event => event.parentIds.length === 0);
+    if (rootEvent) dispatch(setCurrentEvent({ currentEvent: rootEvent }));
+  }
 
   const handleParentButton = () => {
     if (currentEvent) {
