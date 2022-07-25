a01824a34a70eb892f20d1148f748f1c245a14ca
Przemyslaw Stasiuk <przemyslaw.stasiuk98@gmail.com>
2022-07-25 20:17:44 +0200

    added comments to top level files

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -4,6 +4,9 @@ import rootReducer, { appReducer } from './reducer';
 
 const thunkEnhancer = applyMiddleware(thunkMiddleware);
 
+/**
+ * Redux Store used to keep all data in desktop app
+ */
 export const store = createStore(rootReducer, thunkEnhancer);
 
 export type RootState = ReturnType<typeof appReducer>;
